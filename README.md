e290510f7274876777b5f9fa0e16044b32d0141b
itshazlan <itshazlan@Andris-MacBook-Pro.local>
2020-09-15 10:21:57 +0700

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
-# CS108Plugin
-Cordova plugin for read and write rfid or barcode [Only Android]
+# CS108 Barcode / RFID Reader Cordova Plugin for Android
+
+Use the CS108 Barcode / RFID Reader Cordova Plugin to quickly develop mobile apps for barcode or RFID reading (Android). The development is based on the CS108 Java API.
+
+## Pre-requsite
+
+The development environment consists of the following:
+
+- First we need node.js - Download it here. https://nodejs.org/en/download/
+- Make sure to install the latest version of JDK (Java Development Kit)
+- Make sure you have installed latest version of Android Studio SDK.
+
+## Installation
+
+1. Install **Ionic CLI** via **npm**.
+
+    ```  
+        npm install -g @ionic-cli
+    ```
+    
+2. Install **Cordova** via **npm**.
+
+    ```  
+        npm install -g cordova
+    ```
+    
+3. Clone or download [this source code](https://github.com/hamzahalvanaa/ionic-example-cs108) first, then navigate into the project path and install **package.json** requirements via **npm**.
+
+    ```  
+        npm install
+    ```
+
+4. Next step is have to install cordova android platform.
+
+    ```
+        ionic cordova platform add android
+    ```
+
+5. Afterwards, clone or download this plugin and install using below command via local path.
+
+    ```
+        ionic cordova plugin add <local-path-to-plugin>/cordova-plugin-cs108plugin
+    ```
+
+6. After success adding the plugin, execute the next command to build ionic and running into real device, make sure you have turn on the developer options on your android phone settings and using the same network connection on both PC and phone.
+
+    ```
+        ionic cordova run android -lc --device --no-native-run --host=0.0.0.0 --verbose
+    ```
+
+## Development Tips
+
+1. To perform code changes of your plugin files, open cordova platform project (platforms/android) in your Android Studio IDE, change the gradle version in **gradle-wrapper.properties**.
+
+    ```  
+        distributionUrl=https\://services.gradle.org/distributions/gradle-4.10.3-all.zip
+    ```
+
+2. To prevent error and grant permissions while running the ionic project, use this commands:
+
+    ```  
+        chmod a+x platforms/android/gradlew
+        chmod a+x platforms/android/cordova/build
+    ```
+
+## Supported Platforms
+
+- Android
+
+## Others ##
+If your cordova-android version > 6.3.0, please change repositories flatDir ``` dirs 'libs'  ``` to   ```dirs 'src/main/libs'  ``` and change  dependencies ``` compile 'com.android.support:support-v4:+'   ``` to   ```compile 'com.android.support:support-v4:27.1.0'  ```in the file [cordova-plugin-cs108plugin/src/android/cs108library4a.gradle](https://github.com/hamzahalvanaa/cordova-plugin-cs108plugin/blob/master/src/android/cs108library4a.gradle)
+
+
+## Example
+
+**app.home.html**
+
+```html
+   <ion-col>
+     <ion-button expand="block" size="large" color="light" (click)="connect()">
+      <ion-label>Connect</ion-label>
+     </ion-button>
+   </ion-col>
+
+```
+
+**app.home.ts**
+
+```ts
+import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+declare var window: any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(
+    private platform: Platform
+  ) { }
+
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      this.connect();
+    });
+  }
+
+  connect() {
+    window.plugins.cs108Plugin.connect(function (res) {
+      console.log(res);
+    }, function (err) {
+      console.log(err);
+    });
+  }
+}
+
+```
--- src/android/CS108Plugin.java
+++ src/android/CS108Plugin.java
@@ -2,6 +2,13 @@ package com.nocola.cordova.plugin;
 
 // The native Toast API
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.ScanCallback;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.annotation.Keep;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +24,7 @@ import android.content.Context;
 import android.support.v4.app.Fragment;
 
 // Cordova-required packages
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaWebView;
@@ -27,7 +35,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.csl.cs108library4a.Cs108Connector;
 import com.csl.cs108library4a.Cs108Library4A;
+import com.csl.cs108library4a.ReaderDevice;
+
+import java.util.ArrayList;
 
 public class CS108Plugin extends CordovaPlugin {
     final boolean DEBUG = false;
@@ -55,9 +67,22 @@ public class CS108Plugin extends CordovaPlugin {
 
     private CallbackContext callbackContext;
 
-    public static Cs108Library4A mCs108Library4a;
-    public static Context mContext;
-    public static TextView mLogView;
+    private static CordovaPlugin that;
+    public static Context mContext = that.cordova.getActivity();
+    private static CordovaInterface layout;
+    public static TextView mLogView = new TextView(layout.getContext());
+    public static Cs108Library4A mCs108Library4a = new Cs108Library4A(mContext, mLogView);
+    public static SharedObjects sharedObjects;
+
+   private DeviceScanTask deviceScanTask;
+   private ReaderListAdapter readerListAdapter;
+   private BluetoothAdapter.LeScanCallback mLeScanCallback;
+   private ScanCallback mScanCallback;
+   private ArrayList<ReaderDevice> readersList = sharedObjects.readersList;
+
+   private ArrayList<Cs108Connector.Cs108ScanData> mScanResultList = new ArrayList<>();
+   private Handler mHandler = new Handler();
+   private DeviceConnectTask deviceConnectTask;
 
     /**
      * Constructor.
@@ -104,17 +129,27 @@ public class CS108Plugin extends CordovaPlugin {
     }
 
     public void connect() {
-        final CordovaPlugin that = this;
-        FrameLayout layout = (FrameLayout) webView.getView().getParent();
-        mContext = that.cordova.getActivity();
-        mLogView = new TextView(layout.getContext());
-        mCs108Library4a = new Cs108Library4A(mContext, mLogView);
-        boolean scanning = false;
-        if (scanning == false) {
-            scanning = true;
-            if (mCs108Library4a.scanLeDevice(true) == false) ;
-            else cordova.getActivity().invalidateOptionsMenu();
-        }
+       cordova.getThreadPool().execute(new Runnable() {
+           @Override
+           public void run() {
+               boolean operating = false;
+               if (mCs108Library4a.isBleConnected()) operating = true;
+               if (operating == false && deviceScanTask != null) {
+                   if (deviceScanTask.isCancelled() == false) operating = true;
+               }
+               if (operating == false && deviceConnectTask != null) {
+                   if (deviceConnectTask.isCancelled() == false) operating = true;
+               }
+               if (operating == false) {
+                   deviceScanTask = new DeviceScanTask();
+                   deviceScanTask.execute();
+                   mCs108Library4a.appendToLog("Started DeviceScanTask");
+                   // Send a positive result to the callbackContext
+                   PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+                   callbackContext.sendPluginResult(pluginResult);
+               }
+           }
+       });
     }
 
     public boolean hasPermission() {
@@ -153,4 +188,228 @@ public class CS108Plugin extends CordovaPlugin {
         this.callbackContext = callbackContext;
     }
 
+   private class DeviceScanTask extends AsyncTask<Void, String, String> {
+       private long timeMillisUpdate = System.currentTimeMillis();
+       boolean usbDeviceFound = false;
+       ArrayList<ReaderDevice> readersListOld = new ArrayList<ReaderDevice>();
+       boolean wait4process = false;
+       boolean scanning = false;
+
+       @Override
+       protected String doInBackground(Void... a) {
+           while (isCancelled() == false) {
+               if (wait4process == false) {
+                   Cs108Connector.Cs108ScanData cs108ScanData = mCs108Library4a.getNewDeviceScanned();
+                   if (cs108ScanData != null) mScanResultList.add(cs108ScanData);
+                   if (scanning == false || mScanResultList.size() != 0 || System.currentTimeMillis() - timeMillisUpdate > 10000) {
+                       wait4process = true;
+                       publishProgress("");
+                   }
+               }
+           }
+           return "End of Asynctask()";
+       }
+
+       @Override
+       protected void onProgressUpdate(String... output) {
+           if (scanning == false) {
+               scanning = true;
+               if (mCs108Library4a.scanLeDevice(true) == false) cancel(true);
+               else cordova.getActivity().invalidateOptionsMenu();
+           }
+           boolean listUpdated = false;
+           while (mScanResultList.size() != 0) {
+               Cs108Connector.Cs108ScanData scanResultA = mScanResultList.get(0);
+               mCs108Library4a.appendToLog("Scan Result: " + scanResultA);
+               mScanResultList.remove(0);
+               if (false)
+                   mCs108Library4a.appendToLog("scanResultA.device.getType() = " + scanResultA.device.getType() + ". scanResultA.rssi = " + scanResultA.rssi);
+               if (scanResultA.device.getType() == BluetoothDevice.DEVICE_TYPE_LE && (true || scanResultA.rssi < 0)) {
+                   boolean match = false;
+                   for (int i = 0; i < readersList.size(); i++) {
+                       if (readersList.get(i).getAddress().matches(scanResultA.device.getAddress())) {
+                           ReaderDevice readerDevice1 = readersList.get(i);
+                           int count = readerDevice1.getCount();
+                           count++;
+                           readerDevice1.setCount(count);
+                           readerDevice1.setRssi(scanResultA.rssi);
+                           readersList.set(i, readerDevice1);
+                           listUpdated = true;
+                           match = true;
+                           break;
+                       }
+                   }
+                   if (match == false) {
+                       ReaderDevice readerDevice = new ReaderDevice(scanResultA.device, scanResultA.device.getName(), scanResultA.device.getAddress(), false, "", 1, scanResultA.rssi);
+                       String strInfo = "";
+                       if (scanResultA.device.getBondState() == 12) {
+                           strInfo += "BOND_BONDED\n";
+                       }
+                       readerDevice.setDetails(strInfo + "scanRecord=" + mCs108Library4a.byteArrayToString(scanResultA.scanRecord));
+                       readersList.add(readerDevice);
+                       listUpdated = true;
+                   }
+               } else {
+                   if (true)
+                       mCs108Library4a.appendToLog("deviceScanTask: rssi=" + scanResultA.rssi + ", error type=" + scanResultA.device.getType());
+               }
+           }
+           if (System.currentTimeMillis() - timeMillisUpdate > 10000) {
+               timeMillisUpdate = System.currentTimeMillis();
+               for (int i = 0; i < readersList.size(); i++) {
+                   ReaderDevice readerDeviceNew = readersList.get(i);
+                   boolean matched = false;
+                   for (int k = 0; k < readersListOld.size(); k++) {
+                       ReaderDevice readerDeviceOld = readersListOld.get(k);
+                       if (readerDeviceOld.getAddress().matches(readerDeviceNew.getAddress())) {
+                           matched = true;
+                           if (readerDeviceOld.getCount() >= readerDeviceNew.getCount()) {
+                               readersList.remove(i);
+                               listUpdated = true;
+                               readersListOld.remove(k);
+                           } else readerDeviceOld.setCount(readerDeviceNew.getCount());
+                           break;
+                       }
+                   }
+                   if (matched == false) {
+                       ReaderDevice readerDevice1 = new ReaderDevice(null, null, readerDeviceNew.getAddress(), false, null, readerDeviceNew.getCount(), 0);
+                       readersListOld.add(readerDevice1);
+                   }
+               }
+               if (DEBUG)
+                   mCs108Library4a.appendToLog("Matched. Updated readerListOld with size = " + readersListOld.size());
+               mCs108Library4a.scanLeDevice(false);
+               cordova.getActivity().invalidateOptionsMenu();
+               scanning = false;
+           }
+           if (listUpdated) readerListAdapter.notifyDataSetChanged();
+           wait4process = false;
+       }
+
+       @Override
+       protected void onCancelled() {
+           super.onCancelled();
+           mCs108Library4a.appendToLog("Stop Scanning 1A");
+           deviceScanEnding();
+       }
+
+       @Override
+       protected void onPostExecute(String result) {
+           mCs108Library4a.appendToLog("Stop Scanning 1B");
+           deviceScanEnding();
+       }
+
+       void deviceScanEnding() {
+           mCs108Library4a.scanLeDevice(false);
+       }
+   }
+
+   long connectTimeMillis;
+   boolean bConnecting = false;
+
+   private class DeviceConnectTask extends AsyncTask<Void, String, Integer> {
+       private int position;
+       private final ReaderDevice connectingDevice;
+       private String prgressMsg;
+       int waitTime;
+       private CustomProgressDialog progressDialog;
+       private int setting;
+
+       DeviceConnectTask(int position, ReaderDevice connectingDevice, String prgressMsg) {
+           this.position = position;
+           this.connectingDevice = connectingDevice;
+           this.prgressMsg = prgressMsg;
+       }
+
+       @Override
+       protected void onPreExecute() {
+           super.onPreExecute();
+
+           mCs108Library4a.appendToLog("start of Connection with mrfidToWriteSize = " + mCs108Library4a.mrfidToWriteSize());
+           mCs108Library4a.connect(connectingDevice);
+           waitTime = 20;
+           setting = -1;
+           progressDialog = new CustomProgressDialog(cordova.getActivity(), prgressMsg);
+           progressDialog.show();
+       }
+
+       @Override
+       protected Integer doInBackground(Void... a) {
+           do {
+               try {
+                   Thread.sleep(500);
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               }
+               publishProgress("kkk ");
+               if (mCs108Library4a.isBleConnected()) {
+                   setting = 0;
+                   break;
+               }
+           } while (--waitTime > 0);
+           if (progressDialog.isShowing())
+               progressDialog.dismiss();
+           if (setting != 0 || waitTime <= 0) {
+               cancel(true);
+           }
+           publishProgress("mmm ");
+           return waitTime;
+       }
+
+       @Override
+       protected void onProgressUpdate(String... output) {
+       }
+
+       @Override
+       protected void onCancelled(Integer result) {
+           if (true)
+               mCs108Library4a.appendToLog("onCancelled(): setting = " + setting + ", waitTime = " + waitTime);
+           if (setting >= 0) {
+               Toast.makeText(cordova.getActivity(), "Setup problem after connection. Disconnect", Toast.LENGTH_SHORT).show();
+           } else {
+               mCs108Library4a.isBleConnected();
+               Toast.makeText(cordova.getActivity(), "Unable to connect device", Toast.LENGTH_SHORT).show();
+           }
+           super.onCancelled();
+           mCs108Library4a.disconnect(false);
+           mCs108Library4a.appendToLog("done");
+
+           bConnecting = false;
+       }
+
+       protected void onPostExecute(Integer result) {
+           if (DEBUG)
+               mCs108Library4a.appendToLog("onPostExecute(): setting = " + setting + ", waitTime = " + waitTime);
+           ReaderDevice readerDevice = readersList.get(position);
+           readerDevice.setConnected(true);
+           readersList.set(position, readerDevice);
+           readerListAdapter.notifyDataSetChanged();
+
+           String connectedBleAddress = connectingDevice.getAddress();
+           if (connectedBleAddress.matches(sharedObjects.connectedBleAddressOld) == false)
+               sharedObjects.versioinWarningShown = false;
+           sharedObjects.connectedBleAddressOld = connectedBleAddress;
+           sharedObjects.barsList.clear();
+           sharedObjects.tagsList.clear();
+           sharedObjects.tagsIndexList.clear();
+
+           Toast.makeText(cordova.getActivity().getApplicationContext(), "BLE is connected", Toast.LENGTH_SHORT).show();
+
+           connectTimeMillis = System.currentTimeMillis();
+           super.onPostExecute(result);
+           cordova.getActivity().onBackPressed();
+           bConnecting = false;
+           mCs108Library4a.appendToLog("end of Connection with mrfidToWriteSize = " + mCs108Library4a.mrfidToWriteSize());
+       }
+   }
+
+   public class CustomProgressDialog extends ProgressDialog {
+       public CustomProgressDialog(Context context, String message) {
+           super(context, ProgressDialog.STYLE_SPINNER);
+           if (message == null) message = "Progressing. Please wait.";
+           setTitle(null);
+           setMessage(message);
+           setCancelable(false);
+       }
+   }
 }
